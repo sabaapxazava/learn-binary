52aa8cb26d0320033ce1e8d40e7f9142f59aca59
sabaapxazava <sabaapxazava@gmail.com>
2024-01-01 20:49:05 +0400

    feat: Initialize Vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./styles/App.css";
 import Layout from "./components/Layout";
 import { MainRoutes } from "./routes";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <Layout>
       <MainRoutes></MainRoutes>
+      <Analytics />
     </Layout>
   );
 }
